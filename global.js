0ed9ca1a610295676b83fffc8aa1f7754a283d49
Zaiyi Kuang <107820340+derekkuang@users.noreply.github.com>
2025-04-16 16:19:13 -0700

    rework nav link so nav bar exist success

--- global.js
+++ global.js
@@ -1,17 +1,16 @@
 console.log("IT’S ALIVE!");
 
-const BASE_PATH =
-  location.hostname === "localhost" || location.hostname === "127.0.0.1"
-    ? "/" // Local server
-    : "/profolio-dsc106/"; // GitHub Pages repo name
-
 function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
 
-const pages = [
+const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
+  ? "/profolio/"                  // Local server
+  : "/profolio-dsc106/";         // GitHub Pages repo name
+
+let pages = [
   { url: "", title: "Home" },
-  { url: "contact/index.html", title: "Contact" },
+  { url: "contact/", title: "Contact" },
   { url: "projects/", title: "Projects" },
   { url: "resume/", title: "Resume" },
   { url: "https://github.com/derekkuang", title: "Github Profile" },
@@ -23,24 +22,8 @@ document.body.prepend(nav);
 for (let p of pages) {
   let url = p.url;
   let title = p.title;
-
-  url = !url.startsWith("http") ? BASE_PATH + url : url;
-
-  let a = document.createElement("a");
-  a.href = url;
-  a.textContent = title;
-
-  a.classList.toggle(
-    "current",
-    a.host === location.host && a.pathname === location.pathname
-  );
-
-  if (a.host !== location.host) {
-    a.target = "_blank";
-    a.rel = "noopener noreferrer";
-  }
-
-  nav.append(a);
+  url = !url.startsWith('http') ? BASE_PATH + url : url;
+  nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
 }
 
-const navLinks = $$("nav a");
+
